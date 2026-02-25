0b64da81e4bf80b856cc16909512864c26128356
augustin <augustin.denuce@gmail.com>
2026-02-25 16:57:17 +0100

    minimal server node js

--- app/server.js
+++ app/server.js
@@ -1,3 +1,36 @@
+const http = require('http');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const fs = require("fs");
 
+// easy get/post/... request
+const express = require('express');
+
+// database
+const sqlite3 = require('sqlite3');
+
+// unused ?
+const sha256 = require('js-sha256');
+const path = require('path');
+const { fileURLToPath, url } = require('url');
+
+
+let app = express();
+const port = 8000;
+
+
+// get where to find url
+const { apiRoutes, authRoutes }  = require("./api.js");
+
+// activate
+app.use("/api",  apiRoutes);
+app.use("/auth", authRoutes);
+
+// static directory
+app.use(express.static("public"));
+app.use(express.static("share"));
+
+// start server
+app.listen(port, () => { console.log("Server running on http://localhost:" + port) });
 
 
