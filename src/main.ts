14f31ee1de74774c945b3dacc2b2f942f7be74b1
quentin <donierquen@cy-tech.fr>
2026-03-30 12:03:46 +0200

    ajout framework  vue, transpiler ts et systeme authentification

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import { useAuthStore } from '@/auth/auth'
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+
+
+
+async function init(){
+    const auth=useAuthStore()
+    await auth.fakeLogin('teacher')
+    app.mount('#app')
+}
+
+
+init()
